c37b2e6a5f9488bfaaeaade5c5351a995ba1b7c8
terafin <jwood@me.com>
2019-09-06 13:35:26 -0700

    Update mac_install.sh

--- mac_install.sh
+++ mac_install.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 echo "Making the mods directory"
-mkdir -p ~/Library/Application\ Support\minecraft\mods
-cd ~/Library/Application\ Support\minecraft\mods
+mkdir -p ~/Library/Application\ Support/minecraft/mods
+cd ~/Library/Application\ Support/minecraft/mods
 echo "Downloading mods"
 curl -sS https://github.com/terafin/minecraft-mod-collection/raw/master/mods.zip > mods.zip
 echo "Installing mods"
