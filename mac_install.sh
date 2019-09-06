62f2653742c20fad21e3737d112b9fd79462969a
terafin <jwood@me.com>
2019-09-06 13:32:02 -0700

    Update mac_install.sh

--- mac_install.sh
+++ mac_install.sh
@@ -3,5 +3,5 @@
 mkdir -p ~/Library/Application\ Support\minecraft\mods
 cd ~/Library/Application\ Support\minecraft\mods
 curl -sS https://github.com/terafin/minecraft-mod-collection/raw/master/mods.zip > mods.zip
-unzip file.zip
-rm file.zip
+unzip mods.zip
+rm mods.zip
